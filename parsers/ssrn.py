f0609082f34b56937f71e5d56dd5e74045f9c351
Haoyan Huo <hhaoyann@gmail.com>
2020-07-29 21:45:24 -0700

    Add SSRN parser.

--- parsers/ssrn.py
+++ parsers/ssrn.py
@@ -10,27 +10,27 @@ from base import Parser, VespaDocument, indexes
 latest_version = 1
 
 
-class NBERDocument(VespaDocument):
+class SSRNDocument(VespaDocument):
     meta = {
-        "collection": "Scraper_nber_org_parsed_vespa",
+        "collection": "Scraper_papers_ssrn_com_parsed_vespa",
         "indexes": indexes
     }
 
     latest_version = latest_version
-    unparsed_document = ReferenceField('UnparsedNBERDocument', required=True)
+    unparsed_document = ReferenceField('UnparsedSSRNDocument', required=True)
 
 
-class NBERParser(Parser):
-    """nber.org"""
+class SSRNParser(Parser):
+    """ssrn.com"""
 
     def _parse_title(self, doc):
         return doc['Title']
 
     def _parse_authors(self, doc):
-        return [{'name': x} for x in doc['Authors']]
+        return [{'name': x['Name']} for x in doc['Authors']]
 
     def _parse_journal(self, doc):
-        return 'NBER'
+        return 'SSRN'
 
     def _parse_publication_date(self, doc):
         return doc['Publication_Date']
@@ -39,10 +39,10 @@ class NBERParser(Parser):
         return doc['Abstract']
 
     def _parse_origin(self, doc):
-        return "Scraper_nber_org"
+        return "Scraper_papers_ssrn_com"
 
     def _parse_source_display(self, doc):
-        return 'The National Bureau of Economic Research'
+        return 'Social Science Research Network'
 
     def _parse_link(self, doc):
         return doc['Link']
@@ -54,21 +54,18 @@ class NBERParser(Parser):
         return doc['last_updated']
 
     def _parse_has_full_text(self, doc):
-        return doc['fulltext'] is not None
+        return False
 
     def _parse_body_text(self, doc):
-        return [{'section_heading': None, 'text': t} for t in doc['fulltext']]
+        return None
 
     def _parse_doi(self, doc):
         return doc['Doi']
 
-    def _parse_keywords(self, doc):
-        return None
-
     def _parse_license(self, doc):
-        return '© %d by %s' % (doc['Publication_Date'].year, ', '.join(doc['Authors']))
+        return '© %d by the authors' % (doc['Publication_Date'].year,)
 
-    _parse_category_human = _parse_summary_human = \
+    _parse_category_human = _parse_keywords = _parse_summary_human = \
         _parse_issn = _parse_journal_short = _parse_references = \
         _parse_cited_by = _parse_pmcid = _parse_pubmed_id = \
         _parse_who_covidence = lambda self, doc: None
@@ -84,53 +81,27 @@ class NBERParser(Parser):
         return latest_version
 
     def _parse_copyright(self, doc):
-        return "NBER working papers are circulated for discussion and comment purposes. " \
-               "They have not been peer-reviewed or been subject to the review by the NBER " \
-               "Board of Directors that accompanies official NBER publications."
+        return None
 
 
-class NBERFullText(DynamicDocument):
+class UnparsedSSRNDocument(DynamicDocument):
     meta = {
-        "collection": "Scraper_nber_org_fs.files"
-    }
-    filename = StringField(required=True)
-    md5 = StringField(required=True)
-    chunkSize = IntField(required=True)
-    length = LongField(required=True)
-    uploadDate = DateTimeField(required=True)
-
-    pdf_extraction_success = BooleanField()
-    pdf_extraction_plist = ListField()
-    pdf_extraction_exec = StringField()
-    pdf_extraction_version = StringField()
-    parsed_date = DateTimeField()
-
-    def get_paragraphs(self):
-        if not self.pdf_extraction_success:
-            return None
-        return [x['text'] for x in self.pdf_extraction_plist]
-
-
-class UnparsedNBERDocument(DynamicDocument):
-    meta = {
-        "collection": "Scraper_nber_org"
+        "collection": "Scraper_papers_ssrn_com"
     }
 
-    parser = NBERParser()
-    parsed_class = NBERDocument
+    parser = SSRNParser()
+    parsed_class = SSRNDocument
 
-    fulltext = ReferenceField(NBERFullText, db_field='PDF_gridfs_id')
-    parsed_document = ReferenceField(NBERDocument, required=False)
+    parsed_document = ReferenceField(SSRNDocument, required=False)
     last_updated = DateTimeField()
 
     def parse(self):
         doc = self.to_mongo()
-        doc['fulltext'] = self.fulltext.get_paragraphs()
 
         parsed_document = self.parser.parse(doc)
         parsed_document['_bt'] = datetime.now()
         parsed_document['unparsed_document'] = self
-        return NBERDocument(**parsed_document)
+        return SSRNDocument(**parsed_document)
 
 
 if __name__ == '__main__':
@@ -144,7 +115,7 @@ if __name__ == '__main__':
         authentication_source=os.getenv("COVID_DB"),
     )
 
-    for i in UnparsedNBERDocument.objects.all():
+    for i in UnparsedSSRNDocument.objects.all():
         parsed = i.parse()
         parsed.save()
         break
